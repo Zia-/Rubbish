de833bd6290bcbb4ee620b7d8eba4e82c25c9975
Mohammed Zia <mohammed.zia33@gmail.com>
2016-08-12 10:55:48 +0300

    Update populateCityCentroidTable.sql

--- populateCityCentroidTable.sql
+++ populateCityCentroidTable.sql
@@ -8,3 +8,10 @@ SELECT AddGeometryColumn ('citycentroidcoords','geom',4326,'POINT',2);
 
 insert into citycentroidcoords (name, geom) VALUES
 ('zia', ST_SetSRID(ST_MakePoint(-71.1043443253471, 42.3150676015829),4326))
+
+
+--- Insert bbox for each polygon of worldcitypolygon shapefile
+
+SELECT AddGeometryColumn ('worldcitypolygon','geombbox',4326,'POLYGON',2);
+
+insert into worldcitypolygon(geombbox) select st_envelope(geom) from worldcitypolygon
